b3a7ec06627b53edad673de65cea68859095b5d5
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2025-05-25 12:39:40 +1000

    Add project files.

---
+++ ProtobufMSBuild/ProtoC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ProtobufMSBuild
+{
+	public class ProtoC : Task
+	{
+		[Required]
+		string Protobufs { get; set; }
+
+		[Output]
+		string CompiledProtobufs { get; set; }
+
+		public override bool Execute()
+		{
+			//CompiledProtobufs = "test";
+			Log.LogWarning(CompiledProtobufs);
+
+			return Log.HasLoggedErrors;
+			//throw new NotImplementedException();
+		}
+	}
+}
