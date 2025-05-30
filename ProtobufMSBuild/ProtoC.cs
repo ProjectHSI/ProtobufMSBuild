63848e7cbee1bea409ced2616acf94ef9286e1d5
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2025-05-30 15:17:21 +1000

    update to 1.2.1

--- ProtobufMSBuild/ProtoC.cs
+++ ProtobufMSBuild/ProtoC.cs
@@ -1,23 +1,126 @@
-﻿using System;
-using Microsoft.Build.Framework;
+﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System;
+using System.Diagnostics;
 
 namespace ProtobufMSBuild
 {
-	public class ProtoC : Task
+	internal static class SharedProtobufMSBuild {
+		internal static string GetProtobufFile(string file, string projectRoot, string compiledProtobufsDirectory)
+		{
+			return System.IO.Path.Combine(System.IO.Path.Combine(projectRoot, compiledProtobufsDirectory), System.IO.Path.GetFileNameWithoutExtension(file) + ".cs");
+		}
+
+		internal static string Transform(string protobufs, string projectRoot, string compiledProtobufsDirectory)
+		{
+			if (string.IsNullOrEmpty(protobufs)) return string.Empty;
+			var files = protobufs.Split(';');
+			for (int i = 0; i < files.Length; i++)
+			{
+				files[i] = GetProtobufFile(files[i], projectRoot, compiledProtobufsDirectory);
+			}
+			return string.Join(";", files);
+		}
+	}
+
+	public class ProtobufMSBuildDetermine : Task
 	{
 		[Required]
-		string Protobufs { get; set; }
+		public string Protobufs { get; set; }
+
+		[Required]
+		public string ProjectRoot { get; set; }
+
+		[Required]
+		public string ProtobufCompiledDirectory { get; set; }
 
 		[Output]
-		string CompiledProtobufs { get; set; }
+		public string CompiledProtobufs { get; set; }
 
 		public override bool Execute()
 		{
-			//CompiledProtobufs = "test";
-			Log.LogWarning(CompiledProtobufs);
+			//CompiledProtobufs = Protobufs + ".cs";
+			//Log.LogWarning(CompiledProtobufs);
+			CompiledProtobufs = SharedProtobufMSBuild.Transform(Protobufs, ProjectRoot, ProtobufCompiledDirectory);
 
-			return Log.HasLoggedErrors;
+			return !Log.HasLoggedErrors;
+			//throw new NotImplementedException();
+		}
+	}
+
+	public class ProtobufMSBuildCompile : Task
+	{
+		[Required]
+		public string Protobufs { get; set; }
+
+		[Required]
+		public string ProtobufCompiledDirectory { get; set; }
+
+		[Required]
+		public string ProtoCompiler { get; set; }
+
+		[Required]
+		public string ProjectRoot { get; set; }
+
+		[Output]
+		public string CompiledProtobufs { get; set; }
+
+		public override bool Execute()
+		{
+			CompiledProtobufs = SharedProtobufMSBuild.Transform(Protobufs, ProjectRoot, ProtobufCompiledDirectory);
+
+			foreach (var file in Protobufs.Split(';'))
+			{
+				if (!System.IO.File.Exists(file))
+				{
+					Log.LogError($"Protobuf file '{file}' does not exist.");
+				}
+
+				Log.LogMessage($"Compiling Protobuf file: {file}");
+
+				using (Process protocProcess = new Process())
+				{
+					protocProcess.StartInfo.UseShellExecute = false;
+					protocProcess.StartInfo.FileName = ProtoCompiler;
+					protocProcess.StartInfo.CreateNoWindow = true;
+					protocProcess.StartInfo.Arguments = $"-I{ProjectRoot} --csharp_out {ProtobufCompiledDirectory} {System.IO.Path.Combine(ProjectRoot, file)}";
+					protocProcess.StartInfo.WorkingDirectory = ProjectRoot;
+					protocProcess.StartInfo.RedirectStandardOutput = true;
+					protocProcess.StartInfo.RedirectStandardError = true;
+
+					protocProcess.OutputDataReceived += (sender, e) =>
+					{
+						if (!string.IsNullOrEmpty(e.Data))
+						{
+							Log.LogMessage(MessageImportance.Normal, e.Data);
+						}
+					};
+
+					protocProcess.ErrorDataReceived += (sender, e) =>
+					{
+						if (!string.IsNullOrEmpty(e.Data))
+						{
+							Log.LogWarning(e.Data);
+						}
+					};
+
+					Log.LogMessage(MessageImportance.Low, $"{protocProcess.StartInfo.FileName} {protocProcess.StartInfo.Arguments}");
+					protocProcess.Start();
+
+					protocProcess.WaitForExit();
+
+					if (protocProcess.ExitCode != 0)
+					{
+						Log.LogError($"Protobuf compilation failed for file '{file}'. Exit code: {protocProcess.ExitCode}");
+					}
+					else
+					{
+						Log.LogMessage($"Successfully compiled Protobuf file: {file}");
+					}
+				}
+			}
+
+			return !Log.HasLoggedErrors;
 			//throw new NotImplementedException();
 		}
 	}
--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # ProtobufMSBuild
 
-## Important Notice
+## How to Use
 
-Hey all!
-
-I really appreciate people actually using this package, to all the 27 people/projects that use this, thank you!
-
-However, this package is **not ready.** It ***doesn't work.***
-Hopefully, when I have the time working on my project that *does* need this, I'll be able to make this viable for use.
-
-Thank you for your euthasium!
-This package will not do anything if installed, so you may keep this package in your dependencies list - your build will not fail.
+1. Install "Protoc". See the Protobuf Documentation (at this time `Protobuf.Compiler.Tools` does **not** work).
+2. Add to project
+3. Create a .proto file. If you really want to, set the file type to `Protobuf`, but this is not required.
+3. Import the resulting C# namespace (defined in the Protobuf file).
