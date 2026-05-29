17a2b0568902a6effa560eee504348839c056023
Project_HSI <projecthsi@outlook.com>
2026-05-29 14:57:28 +1000

    v2.0.0

--- ProtobufMSBuild/ProtoC.cs
+++ ProtobufMSBuild/ProtoC.cs
@@ -1,23 +1,100 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace ProtobufMSBuild
 {
 	internal static class SharedProtobufMSBuild {
-		internal static string GetProtobufFile(string file, string projectRoot, string compiledProtobufsDirectory)
+		internal struct ProtobufLanguageInfo
 		{
-			return System.IO.Path.Combine(System.IO.Path.Combine(projectRoot, compiledProtobufsDirectory), System.IO.Path.GetFileNameWithoutExtension(file) + ".cs");
+			// %S = Sentence Case File Name
+			// %s = File Name (no modification)
+			// %f = Folder
+			public string FileName { get; }
+
+			public ProtobufLanguageInfo(string fileName)
+			{
+				FileName = fileName;
+			}
 		}
 
-		internal static string Transform(string protobufs, string projectRoot, string compiledProtobufsDirectory)
+		internal static ImmutableDictionary<string, IList<ProtobufLanguageInfo>> ProtobufLanguageToFileType = new Dictionary<string, IList<ProtobufLanguageInfo>>(StringComparer.OrdinalIgnoreCase)
+		{
+			{ "csharp", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%S.cs") } },
+
+			{ "cpp", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%f%s.pb.cc"), new ProtobufLanguageInfo("%f%s.pb.h") } },
+			{ "java", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%f%s/%S.java") } },
+			//{ "kotlin", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%s.java"), new ProtobufLanguageInfo("%s.kt") } }, // Note: Protobuf's Kotlin plugin generates both .kt and .java files
+			{ "python", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%f%s_pb2.py") } },
+			{ "objc", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%f%S.pbobjc.m"), new ProtobufLanguageInfo("%f%S.pbobjc.h") } },
+			//{ "php", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%s.php") } }, // Note: Protobuf's PHP generator uses message names for file names.
+			{ "ruby", new List<ProtobufLanguageInfo> { new ProtobufLanguageInfo("%f%s_pb.rb") } }
+			// rust seems too complicated rn.
+		}.ToImmutableDictionary();
+
+		// standard 2.0 doesn't have path.GetRelativePath. Naive implementation.
+		internal static string GetRelativePath(string file, string projectRoot)
+		{
+			if (string.IsNullOrEmpty(projectRoot))
+			{
+				throw new ArgumentException("Project root cannot be empty", nameof(projectRoot));
+			}
+
+			// C:\Project\stuff\file.proto -> stuff\file.proto
+
+			// This should squash most of the bugs a user should experience...
+			if (!file.StartsWith(projectRoot, StringComparison.OrdinalIgnoreCase))
+			{
+				throw new ArgumentException($"File '{file}' is not under project root '{projectRoot}'", nameof(file));
+			}
+
+			return file.Replace(projectRoot, string.Empty).TrimStart(Path.DirectorySeparatorChar).TrimStart(Path.AltDirectorySeparatorChar);
+		}
+
+		internal static string TransformProtobufLanguageInfoFileName(string file, string projectRoot, string compiledProtobufsDirectory, string fileNamePattern)
+		{
+			string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+			string sentenceCaseFileName = char.ToUpper(fileNameWithoutExtension[0], System.Globalization.CultureInfo.InvariantCulture) + fileNameWithoutExtension.Substring(1);
+			//string folder = GetRelativePath(file, projectRoot);
+			string folder = Path.GetDirectoryName(file); // already relative lol
+			string transformedFileName = fileNamePattern.Replace("%S", sentenceCaseFileName)
+													    .Replace("%s", fileNameWithoutExtension)
+												   	    .Replace("%f", !string.IsNullOrEmpty(folder) ? folder + Path.DirectorySeparatorChar : string.Empty);
+			return Path.Combine(Path.Combine(projectRoot, compiledProtobufsDirectory), transformedFileName);
+		}
+
+		internal static IList<string> GetProtobufFile(string file, string projectRoot, string compiledProtobufsDirectory, string protobufLanguage)
+		{
+			IList<ProtobufLanguageInfo> extensions;
+			try
+			{
+				 extensions = ProtobufLanguageToFileType[protobufLanguage];
+			} catch (KeyNotFoundException)
+			{
+				throw new ArgumentException($"Unsupported Protobuf language: {protobufLanguage}");
+			}
+			var result = new List<string>();
+			foreach (ProtobufLanguageInfo langInfo in extensions)
+			{
+				result.Add(System.IO.Path.Combine(System.IO.Path.Combine(projectRoot, compiledProtobufsDirectory), TransformProtobufLanguageInfoFileName(file, projectRoot, compiledProtobufsDirectory, langInfo.FileName)));
+			}
+			return result;
+		}
+
+		internal static string Transform(string protobufs, string projectRoot, string compiledProtobufsDirectory, string protobufLanguage)
 		{
 			if (string.IsNullOrEmpty(protobufs)) return string.Empty;
 			var files = protobufs.Split(';');
 			for (int i = 0; i < files.Length; i++)
 			{
-				files[i] = GetProtobufFile(files[i], projectRoot, compiledProtobufsDirectory);
+				files[i] = string.Join(";", from currentLanguage in protobufLanguage.Split(';')
+						                     select string.Join(";", GetProtobufFile(files[i], projectRoot, compiledProtobufsDirectory, currentLanguage)));
 			}
 			return string.Join(";", files);
 		}
@@ -29,6 +106,9 @@ namespace ProtobufMSBuild
 		public string Protobufs { get; set; }
 
 		[Required]
+		public string ProtobufLanguage { get; set; }
+
+		[Required]
 		public string ProjectRoot { get; set; }
 
 		[Required]
@@ -37,11 +117,24 @@ namespace ProtobufMSBuild
 		[Output]
 		public string CompiledProtobufs { get; set; }
 
+		[Output]
+		public ITaskItem[] CSCompiledProtobufsAsTaskItem { get; set; }
+
 		public override bool Execute()
 		{
+			Log.LogMessage(MessageImportance.High, $"ProjectRoot: {ProjectRoot}");
+
 			//CompiledProtobufs = Protobufs + ".cs";
 			//Log.LogWarning(CompiledProtobufs);
-			CompiledProtobufs = SharedProtobufMSBuild.Transform(Protobufs, ProjectRoot, ProtobufCompiledDirectory);
+			CompiledProtobufs = SharedProtobufMSBuild.Transform(Protobufs, ProjectRoot, ProtobufCompiledDirectory, ProtobufLanguage);
+			CSCompiledProtobufsAsTaskItem = (from compiledProtobuf in CompiledProtobufs.Split(';')
+											 where compiledProtobuf.EndsWith(".cs", StringComparison.OrdinalIgnoreCase)
+											 select new TaskItem(compiledProtobuf)).ToArray();
+
+			foreach (var file in CompiledProtobufs.Split(';'))
+			{
+					Log.LogMessage(MessageImportance.High, $"generated: {file}");
+			}
 
 			return !Log.HasLoggedErrors;
 			//throw new NotImplementedException();
@@ -60,7 +153,7 @@ namespace ProtobufMSBuild
 		public string ProtoCompiler { get; set; }
 
 		[Required]
-		public string ProtoLanguage { get; set; }
+		public string ProtobufLanguage { get; set; }
 
 		[Required]
 		public string ProjectRoot { get; set; }
@@ -70,7 +163,9 @@ namespace ProtobufMSBuild
 
 		public override bool Execute()
 		{
-			CompiledProtobufs = SharedProtobufMSBuild.Transform(Protobufs, ProjectRoot, ProtobufCompiledDirectory);
+			Log.LogMessage(MessageImportance.High, ProtobufCompiledDirectory);
+
+			CompiledProtobufs = SharedProtobufMSBuild.Transform(Protobufs, ProjectRoot, ProtobufCompiledDirectory, ProtobufLanguage);
 
 			foreach (var file in Protobufs.Split(';'))
 			{
@@ -79,46 +174,49 @@ namespace ProtobufMSBuild
 					Log.LogError($"Protobuf file '{file}' does not exist.");
 				}
 
-				Log.LogMessage($"Compiling Protobuf file: {file}");
-
-				using (Process protocProcess = new Process())
+				foreach (var protobufLanguage in ProtobufLanguage.Split(';'))
 				{
-					protocProcess.StartInfo.UseShellExecute = false;
-					protocProcess.StartInfo.FileName = ProtoCompiler;
-					protocProcess.StartInfo.CreateNoWindow = true;
-					protocProcess.StartInfo.Arguments = $"-I{ProjectRoot} --${ProtoLanguage}_out {ProtobufCompiledDirectory} {System.IO.Path.Combine(ProjectRoot, file)}";
-					protocProcess.StartInfo.WorkingDirectory = ProjectRoot;
-					protocProcess.StartInfo.RedirectStandardOutput = true;
-					protocProcess.StartInfo.RedirectStandardError = true;
+					Log.LogMessage(MessageImportance.High, $"Compiling Protobuf file for language {protobufLanguage}: {file}");
 
-					protocProcess.OutputDataReceived += (sender, e) =>
+					using (Process protocProcess = new Process())
 					{
-						if (!string.IsNullOrEmpty(e.Data))
+						protocProcess.StartInfo.UseShellExecute = false;
+						protocProcess.StartInfo.FileName = ProtoCompiler;
+						protocProcess.StartInfo.CreateNoWindow = true;
+						protocProcess.StartInfo.Arguments = $"-I{ProjectRoot} --{protobufLanguage}_out {ProtobufCompiledDirectory} {System.IO.Path.Combine(ProjectRoot, file)}";
+						protocProcess.StartInfo.WorkingDirectory = ProjectRoot;
+						protocProcess.StartInfo.RedirectStandardOutput = true;
+						protocProcess.StartInfo.RedirectStandardError = true;
+
+						protocProcess.OutputDataReceived += (sender, e) =>
 						{
-							Log.LogMessage(MessageImportance.Normal, e.Data);
-						}
-					};
+							if (!string.IsNullOrEmpty(e.Data))
+							{
+								Log.LogMessage(MessageImportance.Normal, e.Data);
+							}
+						};
 
-					protocProcess.ErrorDataReceived += (sender, e) =>
-					{
-						if (!string.IsNullOrEmpty(e.Data))
+						protocProcess.ErrorDataReceived += (sender, e) =>
 						{
-							Log.LogWarning(e.Data);
+							if (!string.IsNullOrEmpty(e.Data))
+							{
+								Log.LogWarning(e.Data);
+							}
+						};
+
+						Log.LogCommandLine(MessageImportance.Low, $"{protocProcess.StartInfo.FileName} {protocProcess.StartInfo.Arguments}");
+						protocProcess.Start();
+
+						protocProcess.WaitForExit();
+
+						if (protocProcess.ExitCode != 0)
+						{
+							Log.LogError($"Protobuf compilation failed for file '{file}' and language {protobufLanguage}. Exit code: {protocProcess.ExitCode}");
 						}
-					};
-
-					Log.LogMessage(MessageImportance.Low, $"{protocProcess.StartInfo.FileName} {protocProcess.StartInfo.Arguments}");
-					protocProcess.Start();
-
-					protocProcess.WaitForExit();
-
-					if (protocProcess.ExitCode != 0)
-					{
-						Log.LogError($"Protobuf compilation failed for file '{file}'. Exit code: {protocProcess.ExitCode}");
-					}
-					else
-					{
-						Log.LogMessage($"Successfully compiled Protobuf file: {file}");
+						else
+						{
+							Log.LogMessage($"Successfully compiled Protobuf file for language {protobufLanguage}: {file}");
+						}
 					}
 				}
 			}
--- README.md
+++ README.md
@@ -2,8 +2,41 @@
 
 ## How to Use
 
-1. Install "Protoc". See the Protobuf Documentation (at this time `Protobuf.Compiler.Tools` does **not** work).
+1. Install `protoc`. See the Protobuf Documentation.
 2. Add to project
-3. Create a .proto file. If you really want to, set the file type to `Protobuf`, but this is not required.
+3. Create a `.proto` file. If you really want to, set the file type to `Protobuf`, but this is not required.
 4. Build the project - it doesn't matter whether it has compile-time issues or not, it just needs to be built to run the "ProtobufCompile" target.
-5. Import the resulting C# namespace (defined in the Protobuf file).
+
+If you use C#, import the resulting C# namespace (defined in the Protobuf file).
+
+For any other language, add the generated files to your project in your project file (I.E. with `ClCompile`). The resulting file names are consistent with that of the `protoc` tool.
+
+## Supported Languages
+
+| Language           | Supported | Additional Notes                                                                   |
+| ------------------ | --------- | ---------------------------------------------------------------------------------- |
+| csharp             | Yes       | Default when auto-included.                                                        |
+| cpp                | Yes       |                                                                                    |
+| dart               | No        | Does not work with current command line arguments. May be supported in the future. |
+| go                 | No        | Does not work with current command line arguments. May be supported in the future. |
+| java               | Yes       |                                                                                    |
+| kotlin             | No        | Untested                                                                           |
+| python             | Yes       |                                                                                    |
+| objc (Objective-C) | Yes       |                                                                                    |
+| php                | No        | Supporting requires `.proto` parsing.                                              |
+| ruby               | Yes       |                                                                                    |
+| rust               | No        | Output format not well specified.                                                  |
+
+## Properties
+
+| Option Name                           | Description                                           | Default Value                             |
+| ------------------------------------- | ----------------------------------------------------- | ----------------------------------------- |
+| `NoProtobufMSBuildAutoInclude`        | Prevents automatic inclusion of .proto files          | N/A (false)                               |
+| `NoProtobufMSBuildAutoIncludeCompile` | Prevents automatic inclusion of compiled .proto files | N/A (false)                               |
+| `ProtobufCompiledDirectory`           | Directory for compiled .proto files                   | `$(IntermediateOutputPath)ProtobufOutput` |
+
+## Item Metadata
+
+| Metadata Name      | Description                                           | Default Value                             |
+| ------------------ | ----------------------------------------------------- | ----------------------------------------- |
+| `ProtobufLanguage` | The language to compile the .proto file for.          | N/A (C# if included by auto-include)      |
